36806f1f9cd508b27a591770780665b814b863ff
shaan Yadav <shaanyworkspace@gmail.com>
2025-02-12 15:31:31 +0530

    Now , application only work with database user , but not involved password incription

--- src/main/java/com/journalapp/service/UserDetailService.java
+++ src/main/java/com/journalapp/service/UserDetailService.java
@@ -1,5 +1,9 @@
 package com.journalapp.service;
 
+import com.journalapp.Repository.UserRepository;
+import com.journalapp.entity.UserPrincipal;
+import com.journalapp.entity.UsersEntry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UsersEntry user = userRepository.findByUsername(username);
+        if(user ==null){
+            System.out.println("No user found");
+            //let the system know by the exception
+            throw new UsernameNotFoundException("User Not found");
+        }
+
+        //If there is a match in password this will be passed from it
+        return new UserPrincipal(user);
     }
 }
